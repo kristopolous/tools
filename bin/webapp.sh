5f583f99fcc6b7d2231b22febfc8cf3b2af39b6a
Chris McKenzie <kristopolous@yahoo.com>
2016-04-12 04:03:08 -0700

    fixing the web app shell

--- bin/webapp.sh
+++ bin/webapp.sh
@@ -11,15 +11,8 @@ make_bins() {
   done
 }
 
-echo $ex
 if [ "$ex" == "webapp.sh" ]; then
   make_bins
 else
-  for i in slack hipchat skype; do
-    if [ "$0" == "$i" ]; then
-      echo google-chrome --app=https://$i/
-      exit
-    fi
-  done
-  echo "Woops, don't know how to start $ex"
+  google-chrome --app=https://${!ex}/ &
 fi
