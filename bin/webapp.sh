fc89d0c80587ac13467c6d726fb7c78bcab6ff9d
Chris McKenzie <kristopolous@yahoo.com>
2016-04-13 11:04:37 -0700

    adding more web apps and unifying the code

--- bin/webapp.sh
+++ bin/webapp.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 ex=`basename $0`
-slack=waivecar.slack.com
-hipchat=crowdfunder.hipchat.com/chat
-skype=web.skype.com
+
+declare -A url_list
+url_list[slack]=waivecar.slack.com
+url_list[hipchat]=crowdfunder.hipchat.com/chat
+url_list[skype]=web.skype.com
+url_list[hangouts]=hangouts.google.com
 
 make_bins() {
-  for i in slack hipchat skype; do
-    [ -e $i ] || ln -s $PWD/webapp.sh $i
+  for i in "${!url_list[@]}"; do
+    if [ -e $i ]; then
+      echo "$i already exists"
+    else
+      echo "Creating $i"
+      ln -s $PWD/webapp.sh $i
+    fi
   done
 }
 
 if [ "$ex" == "webapp.sh" ]; then
   make_bins
 else
-  google-chrome --app=https://${!ex}/
+  google-chrome --app=https://${url_list[$ex]}/
 fi
