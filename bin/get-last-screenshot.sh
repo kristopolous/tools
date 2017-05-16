03b855620eac84e914ca1579555e1dc433799f3c
Chris McKenzie <kristopolous@yahoo.com>
2017-05-16 15:36:25 -0700

    Adding a way to get n-1 screens

--- bin/get-last-screenshot.sh
+++ bin/get-last-screenshot.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-path=`adb shell "ls /mnt/sdcard/Pictures/Screenshots/Sc* | tail -1" | tr -d '\r\n'`
+#!/bin/bash
+which=${1:-1}
+path=`adb shell "ls /mnt/sdcard/Pictures/Screenshots/Sc* | tail -$which | head -1" | tr -d '\r\n'`
 adb pull $path
