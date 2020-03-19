6860b6f6cd2f32f11ca0a63116eaee5a6aa79d85
Chris McKenzie <kristopolous@yahoo.com>
2020-03-18 20:26:57 -0700

    more signal work

--- bin/signal-log.sh
+++ bin/signal-log.sh
@@ -2,26 +2,28 @@
 n=0
 arr=()
 while [ 0 ]; do
+  ping -q -w 2 -c 2 8.8.8.8 > /dev/null
+  arr+=( $? )
+  [[ $count -gt 20 ]] && arr=( ${arr[@]:1} )
+  ttl=0
+  count=0
+  for x in ${arr[@]}; do ttl=$(( x + ttl )); done
+  count=${#arr[@]}
+  last=$(( 100 - ttl * 100 / count ))
+
   stats=$(links -dump http://admin:admin@192.168.1.1/status_deviceinfo.htm | grep -A 2 "SNR Margin" | grep -Po '[\d\.]*' | tr '\n' ' ')
   if [ -n "$stats" ]; then
-    echo  "$n $stats" >> power-history
+    echo  "$n $last $stats" >> power-history
     n=$(( n + 1 ))
     fail=0
   else
     if (( fail < 5 )); then
-      echo "$n 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0" >> power-history
+      echo "$n $last 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0" >> power-history
       sleep 3
       n=$(( n + 1 ))
     fi
     fail=$((fail + 1))
   fi
-  ping -q -w 2 -c 2 8.8.8.8 > /dev/null
-  arr+=( $? )
-  ttl=0
-  for x in ${arr[@]}; do ttl=$(( x + count )); done
-  count=${#arr[@]}
-  [[ $count -gt 10 ]] && arr=( ${arr[@]:1} )
-  echo $count $ttl
 
   tail -900 power-history| sed -E 's/^[0-9]* //g' | awk ' { print FNR" "$0 } ' > /tmp/power
   mv /tmp/power power-history
